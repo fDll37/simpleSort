608ddd18376d99d1820eec063962b2276568f3d1
Данил Менделев <danilmendelev@MBA-fDll.local>
2024-08-26 00:33:01 +0300

    init commit

---
+++ simpleSort/Sort.swift
@@ -0,0 +1,155 @@
+import Foundation
+
+enum SizeArray: Int, CaseIterable {
+    case ten = 10
+    case oneHundred = 100
+//    case oneThousand = 1000
+//    case tenThousand = 10_000
+//    case hundredThousand = 100_000
+//    case million = 1_000_000
+}
+
+class Sort {
+    
+    static var array: [Int] = []
+    static private var asg: UInt64 = 0 // кол-во присвоений
+    static private var cmp: UInt64 = 0 // кол-во сравнений
+    
+    static func bubbleSort() {
+        prepareForStart()
+        let count = array.count
+        
+        for i in stride(from: count - 1, to: 0, by: -1) {
+            let currentAsg = asg
+            for j in 0..<i {
+                if more(array[j], array[j + 1]) {
+                    swap(j, j + 1)
+                }
+            }
+            if asg == currentAsg {
+                break
+            }
+        }
+    }
+
+    static func insertionSort() {
+        prepareForStart()
+        
+        for i in 1..<array.count {
+            var j = i - 1
+            while j >= 0 && more(array[j], array[j + 1]) {
+                swap(j, j + 1)
+                j -= 1
+            }
+            
+        }
+    }
+    
+    static func insertionShiftSort() {
+        prepareForStart()
+        
+        
+        for i in 1..<array.count {
+            let temp = array[i]
+            asg += 1
+            
+            var j = i - 1
+            while j >= 0 && more(array[j], temp) {
+                array[j + 1] = array[j]
+                asg += 1
+                j -= 1
+            }
+            array[j + 1] = temp
+            asg += 1
+        }
+    }
+    
+    static func insertionBinaryShiftSort() {
+        prepareForStart()
+        
+        
+        for i in 1..<array.count {
+            let temp = array[i]
+            let p = binarySearch(key: temp, low: 0, high: i - 1)
+            asg += 1
+            
+            var j = i - 1
+            while j >= p {
+                array[j + 1] = array[j]
+                j -= 1
+            }
+            asg += UInt64(i - p)
+            array[j + 1] = temp
+            asg += 1
+        }
+    }
+    
+
+    static func shellSort() {
+        prepareForStart()
+        
+        var gap = array.count / 2
+        
+        while gap > 0 {
+            for j in gap..<array.count {
+                var i = j
+                while i >= gap && more(array[i - gap], array[i]) {
+                    swap(i - gap, i)
+                    i -= gap
+                }
+            }
+            
+            gap /= 2
+        }
+    }
+    
+    static func getRandomArray(with size: SizeArray) {
+        array = []
+        for _ in 0..<size.rawValue {
+            array.append(Int.random(in: 0...size.rawValue))
+        }
+    }
+    
+    static func getStatistic() {
+        print("N = \(array.count), Перестановок = \(cmp), Присвоений = \(asg)")
+    }
+    
+    static private func binarySearch(key: Int, low: Int, high: Int) -> Int {
+        if high <= low {
+            if moreq(key, array[low]) {
+                return low + 1
+            }
+            return low
+        }
+        
+        let middle = (low + high) / 2
+        if more(key, array[middle]) {
+            return binarySearch(key: key, low: middle + 1, high: high)
+        } else {
+            return binarySearch(key: key, low: low, high: middle - 1)
+        }
+        
+    }
+    
+    static private func prepareForStart() {
+        asg = 0
+        cmp = 0
+    }
+    
+    static private func swap(_ index: Int, _ indexTwo: Int) {
+        asg += 3
+        let temp = array[index]
+        array[index] = array[indexTwo]
+        array[indexTwo] = temp
+    }
+    
+    static private func more(_ value: Int, _ valueTwo: Int) -> Bool {
+        cmp += 1
+        return value > valueTwo
+    }
+    
+    static private func moreq(_ value: Int, _ valueTwo: Int) -> Bool {
+        cmp += 1
+        return value >= valueTwo
+    }
+}
---
+++ simpleSort/Test.swift
@@ -0,0 +1,193 @@
+import Foundation
+
+
+class Test {
+    
+    enum File: String {
+        case input = "in"
+        case output = "out"
+    }
+    
+    static private let fileManager = FileManager.default
+    
+    private init() {}
+    
+    static func run() {
+//        print("==================RANDOM======================")
+//        checkRandom()
+//        print("==================DIGITS======================")
+//        checkDigits()
+        print("==================SORTED======================")
+        checkSorted()
+//        print("==================REVERS======================")
+//        checkRevers()
+    }
+    
+    static private func startBy(size: Int) {
+        var arrayForSort: [Int] = []
+        
+        arrayForSort = Sort.array
+        
+        if size >= 10_000 && size <= 100_000 {
+//            print("start Bubble sort. Array count = \(size)")
+//            var start = Date()
+//            Sort.bubbleSort()
+//            var end = Date()
+//        
+//            Sort.getStatistic()
+//            print("time: \(end.timeIntervalSince(start))")
+//            print("------------------------------------")
+//            
+            Sort.array = arrayForSort
+            print("start Insertion sort. Array count = \(size)")
+            var start = Date()
+            Sort.insertionSort()
+            var end = Date()
+        
+            Sort.getStatistic()
+            print("time: \(end.timeIntervalSince(start))")
+            print("------------------------------------")
+        }
+
+    
+        
+        if size >= 100_000 && size <= 1_000_000 {
+            Sort.array = arrayForSort
+            print("start Insertion SHIFT sort. Array count = \(size)")
+            var start = Date()
+            Sort.insertionShiftSort()
+            var end = Date()
+            
+            Sort.getStatistic()
+            print("time: \(end.timeIntervalSince(start))")
+            print("------------------------------------")
+            
+            Sort.array = arrayForSort
+            print("start Insertion BINARY sort. Array count = \(size)")
+            start = Date()
+            Sort.insertionBinaryShiftSort()
+            end = Date()
+        
+            Sort.getStatistic()
+            print("time: \(end.timeIntervalSince(start))")
+            print("------------------------------------")
+            
+        }
+    
+//        Sort.array = arrayForSort
+//        print("start Shell sort. Array count = \(size)")
+//        var start = Date()
+//        Sort.shellSort()
+//        var end = Date()
+//    
+//        Sort.getStatistic()
+//        print("time: \(end.timeIntervalSince(start))")
+//        print("------------------------------------")
+    }
+    
+    static private func checkRandom() {
+        
+        for index in 0...6 {
+            let inputTestValue = getDataFrom(path: "test/random/test.\(index).\(File.input.rawValue)").split(separator: "\r\n")
+            let countElement = Int(inputTestValue[0]) ?? 0
+            let arrayInput = inputTestValue[1].split(separator: " ").compactMap {Int($0)}
+            
+            Sort.array = arrayInput
+            startBy(size: countElement)
+            
+            let outputTestValue = getDataFrom(path: "test/random/test.\(index).\(File.output.rawValue)").replacingOccurrences(of: "\r\n", with: " ", options: .caseInsensitive).split(separator: " ").compactMap{Int($0)}
+            
+            if Sort.array == outputTestValue {
+                print("Test №\(index) is GOOD\n\n\n")
+            } else {
+                print("Test \(index) is failed")
+                break
+            }
+        }
+    }
+    
+    static private func checkDigits() {
+        
+        for index in 0...6 {
+            let inputTestValue = getDataFrom(path: "test/digits/test.\(index).\(File.input.rawValue)").split(separator: "\r\n")
+            let countElement = Int(inputTestValue[0]) ?? 0
+            let arrayInput = inputTestValue[1].split(separator: " ").compactMap {Int($0)}
+            
+            Sort.array = arrayInput
+            startBy(size: countElement)
+            
+            let outputTestValue = getDataFrom(path: "test/digits/test.\(index).\(File.output.rawValue)").replacingOccurrences(of: "\r\n", with: " ", options: .caseInsensitive).split(separator: " ").compactMap{Int($0)}
+            
+            if Sort.array == outputTestValue {
+                print("Test №\(index) is GOOD\n\n\n")
+            } else {
+                print("Test \(index) is failed")
+                break
+            }
+        }
+    }
+    
+    static private func checkSorted() {
+        
+        for index in 4...6 {
+            let inputTestValue = getDataFrom(path: "test/sorted/test.\(index).\(File.input.rawValue)").split(separator: "\r\n")
+            let countElement = Int(inputTestValue[0]) ?? 0
+            let arrayInput = inputTestValue[1].split(separator: " ").compactMap {Int($0)}
+            
+            Sort.array = arrayInput
+            startBy(size: countElement)
+            
+            let outputTestValue = getDataFrom(path: "test/sorted/test.\(index).\(File.output.rawValue)").replacingOccurrences(of: "\r\n", with: " ", options: .caseInsensitive).split(separator: " ").compactMap{Int($0)}
+            
+            if Sort.array == outputTestValue {
+                print("Test №\(index) is GOOD\n\n\n")
+            } else {
+                print("Test \(index) is failed")
+                break
+            }
+        }
+    }
+    
+    static private func checkRevers() {
+        
+        for index in 0...6 {
+            let inputTestValue = getDataFrom(path: "test/revers/test.\(index).\(File.input.rawValue)").split(separator: "\r\n")
+            let countElement = Int(inputTestValue[0]) ?? 0
+            let arrayInput = inputTestValue[1].split(separator: " ").compactMap {Int($0)}
+            
+            Sort.array = arrayInput
+            startBy(size: countElement)
+            
+            let outputTestValue = getDataFrom(path: "test/revers/test.\(index).\(File.output.rawValue)").replacingOccurrences(of: "\r\n", with: " ", options: .caseInsensitive).split(separator: " ").compactMap{Int($0)}
+            
+            if Sort.array == outputTestValue {
+                print("Test №\(index) is GOOD\n\n\n")
+            } else {
+                print("Test \(index) is failed")
+                break
+            }
+        }
+    }
+    
+    static private func getDataFrom(path: String) -> String {
+        
+        if fileManager.fileExists(atPath: path) {
+            do {
+                let fileContents = try String(contentsOfFile: path, encoding: .utf8)
+                return fileContents
+            } catch {
+                print("Error reading file: \(error.localizedDescription)")
+                return "-2"
+            }
+        } else {
+            print("File does not exist")
+            return "-1"
+        }
+    }
+}
+
+
+
+
+
+
